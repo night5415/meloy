682693e109f974140a4de83fa0b2291c36382440
night5415 <meloymike@gmail.com>
2024-03-28 12:33:29 -0500

    Create Meloy Blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,13 @@
-import React, { ChangeEvent, useState } from "react";
-import AwesomeTextbox from "./components/AwesomeTextbox";
-import AwesomeButton from "./components/AwesomeButton";
-import "./App.css";
-import * as firebase from "firebase/app";
-import "firebase/firebase-database";
-
-interface IUser {
-  UserId: number,
-  FirstName?: string,
-  LastName?: string,
-  Email: string
-}
+import './App.css'
 
 function App() {
-  const [userId, setUserId] = useState(Number),
-    [firstName, setFirstName] = useState<string>(),
-    [lastName, setLastName] = useState<string>(),
-    [email, setEmail] = useState<string>(),
-    pId = (e: ChangeEvent<HTMLInputElement>) => {
-      let id = Number.parseInt(e.target.value);
-      setUserId(id);
-    },
-    pFirst = (e: ChangeEvent<HTMLInputElement>) => {
-      setFirstName(e.target.value);
-    },
-    pLast = (e: ChangeEvent<HTMLInputElement>) => {
-      setLastName(e.target.value);
-    },
-    pEmail = (e: ChangeEvent<HTMLInputElement>) => {
-      setEmail(e.target.value);
-    },
-    onUserDelete = () => {
-      firebase.database().ref(`users/${userId}`)
-        .remove()
-        .then(() => {
-          alert('User Deleted');
-        })
-        .catch(e => {
-          console.error(e);
-        });
-    },
-    onUserSave = () => {
-      const user: IUser = {
-        UserId: userId,
-        FirstName: firstName,
-        LastName: lastName,
-        Email: email!
-      };
-
-      if (Number.isNaN(user.UserId) || !user.UserId) {
-        alert('Failed to save User');
-        return;
-      }
-
-      firebase.database().ref(`users/${userId}`).set(user)
-        .then((t) => {
-          alert('User Saved');
-        })
-        .catch((e) => {
-          alert("Error Saving User");
-        });
-    };
   return (
-    <div className="App">
-      <AwesomeTextbox labelFor="User_Id" onValChange={pId} required={true}></AwesomeTextbox>
-      <AwesomeTextbox labelFor="First_Name" onValChange={pFirst}></AwesomeTextbox>
-      <AwesomeTextbox labelFor="Last_Name" onValChange={pLast}></AwesomeTextbox>
-      <AwesomeTextbox labelFor="Email" onValChange={pEmail} required={true}></AwesomeTextbox>
-      <AwesomeButton text="Save User" onUserClick={onUserSave}></AwesomeButton>
-      <AwesomeButton text="Delete User" onUserClick={onUserDelete}></AwesomeButton>
-    </div>
-  );
+    <>
+      <div>
+      </div>
+      <h1>Working on Something Magical</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
